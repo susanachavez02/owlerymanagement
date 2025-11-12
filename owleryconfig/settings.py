3404fcbecaf331a718fd08699388833a5d2c1648
Susana Chavez <sxusii@outlook.com>
2025-11-12 22:22:10 +0000

    logo addition and navbar change

--- owleryconfig/settings.py
+++ owleryconfig/settings.py
@@ -18,6 +18,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+
 # Load environment variables from .env file
 load_dotenv(BASE_DIR / '.env')
 
@@ -139,6 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
