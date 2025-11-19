cc892027b93654eab17c4f6d7afbdb2cca7af315
Susana Chavez <sxusii@outlook.com>
2025-11-19 18:22:22 +0000

    contract gen nav tab, personalized user dashboards and access

--- owleryconfig/settings.py
+++ owleryconfig/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
 
     # Third-party apps
     'crispy_forms',
